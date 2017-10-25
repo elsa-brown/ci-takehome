8d085416ba97431d6b9bc5a1d232d8ab740e6997
Elsa Brown <elsambrown@gmail.com>
2017-10-24 20:46:45 -0400

    starting point up and running

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {ApolloProvider, createNetworkInterface, ApolloClient} from 'react-apollo';
+import 'tachyons';
+import './index.css';
+
+const networkInterface = createNetworkInterface({
+  uri: 'http://antserver-blocjgjbpw.now.sh/graphql'
+})
+
+const client = new ApolloClient({networkInterface})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app')
+);
